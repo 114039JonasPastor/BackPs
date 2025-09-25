07e6a348276eee7bcf1c70a8966b38f80d8cbd99
114039_PASTORJONAS <114039@tecnicatura.frc.utn.edu.ar>
2025-09-25 18:44:30 -0300

    cambios a usuario

--- App/src/main/java/ar/edu/utn/frc/tup/app/entities/Usuario.java
+++ App/src/main/java/ar/edu/utn/frc/tup/app/entities/Usuario.java
@@ -9,12 +9,18 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @ColumnDefault("nextval('usuarios_idusuario_seq')")
     @Column(name = "idusuario", nullable = false)
@@ -44,4 +50,33 @@ public class Usuario {
     @Column(name = "active", nullable = false)
     private Boolean active = false;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority((role.name()))); //FIXME
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
