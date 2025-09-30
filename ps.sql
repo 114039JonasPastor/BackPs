436a1409178d71650d2ffc98247f675942aff539
114039_PASTORJONAS <114039@tecnicatura.frc.utn.edu.ar>
2025-09-30 17:03:27 -0300

    Update ps.sql

--- ps.sql
+++ ps.sql
@@ -1,16 +1,39 @@
-CREATE DATABASE ps
+CREATE DATABASE Ps
+
 CREATE TABLE Roles (
                        idRol SERIAL PRIMARY KEY,
                        descripcion VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Usuarios (
-                          idUsuario SERIAL PRIMARY KEY,
+CREATE TABLE Auth (
+                          idAuth SERIAL PRIMARY KEY,
                           password VARCHAR(255) NOT NULL,
                           name VARCHAR(255) NOT NULL,
                           lastName VARCHAR(255) NOT NULL,
                           mail VARCHAR(150) NOT NULL UNIQUE,
-			  active BOOLEAN NOT NULL
+			              active BOOLEAN NOT NULL
+);
+
+CREATE TABLE Tipos_Documento (
+                          idTipoDoc SERIAL PRIMARY KEY,
+                          tipo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Departamentos (
+                             idDepartamento SERIAL PRIMARY KEY,
+                             departamento VARCHAR(255)
+);
+
+CREATE TABLE Ciudades (
+                          idCiudad SERIAL PRIMARY KEY,
+                          ciudad VARCHAR(100) NOT null,
+                          idDepartamento INT not null references Departamentos(idDepartamento)
+);
+
+CREATE TABLE Barrios (
+                         idBarrio SERIAL PRIMARY KEY,
+                         barrio VARCHAR(100) NOT NULL,
+                         idCiudad INT NOT NULL REFERENCES Ciudades(idCiudad)
 );
 
 CREATE TABLE Direcciones (
@@ -24,44 +47,25 @@ CREATE TABLE Direcciones (
                              observaciones VARCHAR(200)
 );
 
-CREATE TABLE Tipos_Documento (
-                          idTipoDoc SERIAL PRIMARY KEY,
-                          tipo VARCHAR(255) NOT NULL
+create table Usuarios (
+				idUsuario SERIAL primary key,			    
+			    documento VARCHAR(20),
+                telefono VARCHAR(20),
+			    nacimiento date NOT NULL,
+			    
+                idDireccion INT NOT NULL REFERENCES Direcciones(idDireccion),  
+                idTipoDoc INT NOT NULL REFERENCES Tipos_Documento(idTipoDoc),
+				idAuth INT NOT NULL REFERENCES Auth(idAuth)
 );
+
+
 
 CREATE TABLE RolXUsuario(
                             idRolXUsuario SERIAL PRIMARY KEY,
                             idRol INT NOT NULL REFERENCES Roles(idRol),
-                            idUsuario INT NOT NULL REFERENCES Usuarios(idUsuario)
+                            idAuth INT NOT NULL REFERENCES Usuarios(idUsuario)
 );
 
-
-CREATE TABLE Departamentos (
-                             idDepartamento SERIAL PRIMARY KEY,
-                             departamento VARCHAR(255)
-);
-
-CREATE TABLE Ciudades (
-                          idCiudad SERIAL PRIMARY KEY,
-                          ciudad VARCHAR(100) NOT null,
-                          idDepartamento INT not null references departamentos(idDepartamento)
-);
-
-CREATE TABLE Barrios (
-                         idBarrio SERIAL PRIMARY KEY,
-                         barrio VARCHAR(100) NOT NULL,
-                         idCiudad INT NOT NULL REFERENCES Ciudades(idCiudad)
-);
-
-CREATE TABLE Clientes (
-                          idCliente SERIAL PRIMARY KEY,
-			            idTipoDoc INT NOT NULL REFERENCES Tipos_Documento(idTipoDoc),
-			            documento VARCHAR(20),
-                        telefono VARCHAR(20),
-			            nacimiento TIME NOT NULL,
-                        idUsuario INT NOT NULL REFERENCES Usuarios(idUsuario),
-                        idDireccion INT NOT NULL REFERENCES Direcciones(idDireccion)
-);
 
 CREATE TABLE Oficios (
                          idOficio SERIAL PRIMARY KEY,
@@ -69,16 +73,11 @@ CREATE TABLE Oficios (
 );
 
 CREATE TABLE Profesionales (
-                               idProfesional SERIAL PRIMARY KEY,
-			       idTipoDoc INT NOT NULL REFERENCES Tipos_Documento(idTipoDoc),
-			       documento VARCHAR(20),
-                               telefono VARCHAR(20),
-                               idUsuario INT NOT NULL REFERENCES Usuarios(idUsuario),
-                               idOficio INT NOT NULL REFERENCES Oficios(idOficio),
-			       nacimiento TIME NOT NULL,
-                               fechaDesde TIME NOT NULL,
-                               fechaHasta TIME,
-                               idDireccion INT NOT NULL REFERENCES Direcciones(idDireccion)
+                        idProfesional SERIAL PRIMARY KEY,                      
+                        fechaDesde date NOT NULL,
+                        fechaHasta date,
+                        idUsuario INT NOT NULL REFERENCES Usuarios(idUsuario),
+                        idOficio INT NOT NULL REFERENCES Oficios(idOficio)
 );
 
 CREATE TABLE Disponibilidad (
@@ -91,7 +90,7 @@ CREATE TABLE Disponibilidad (
 
 CREATE TABLE Solicitudes (
                              idSolicitud SERIAL PRIMARY KEY,
-                             idCliente INT NOT NULL REFERENCES Clientes(idCliente),
+                             idUsuario INT NOT NULL REFERENCES Usuario(idUsuario),
                              idProfesional INT NOT NULL REFERENCES Profesionales(idProfesional),
                              idOficio INT NOT NULL REFERENCES Oficios(idOficio),
                              fechaSolicitud TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -102,7 +101,7 @@ CREATE TABLE Solicitudes (
 
 CREATE TABLE Resenias (
                          idResenia SERIAL PRIMARY KEY,
-                         idCliente INT NOT NULL REFERENCES Clientes(idCliente),
+                         idUsuario INT NOT NULL REFERENCES Usuario(idUsuario),
                          idProfesional INT NOT NULL REFERENCES Profesionales(idProfesional),
                          puntuacion INT CHECK (puntuacion BETWEEN 1 AND 5),
                          comentario VARCHAR(500),
@@ -126,6 +125,8 @@ CREATE TABLE Montos (
                         precioMax NUMERIC(10,2) NOT NULL
 );
 
+
+
 CREATE TABLE MediosDePago (
                               idMedioPago SERIAL PRIMARY KEY,
                               descripcion VARCHAR(100) NOT NULL
@@ -133,11 +134,11 @@ CREATE TABLE MediosDePago (
 
 CREATE TABLE Facturas (
                           NroFactura SERIAL PRIMARY KEY,
-                          idCliente INT NOT NULL REFERENCES Clientes(idCliente),
+                          idUsuario INT NOT NULL REFERENCES Usuario(idUsuario),
                           idProfesional INT NOT NULL REFERENCES Profesionales(idProfesional),
                           idMedioPago INT NOT NULL REFERENCES MediosDePago(idMedioPago),
                           fecha TIMESTAMP DEFAULT NOW(),
-                          estadoPago VARCHAR(20) NOT NULL -- pendiente, pagado, cancelado
+                          estadoPago VARCHAR(20) NOT NULL -- pendiente, pagado, cancelado
 );
 
 CREATE TABLE password_reset_tokens (
@@ -150,45 +151,44 @@ CREATE TABLE password_reset_tokens (
     id_usuario INT REFERENCES Usuarios(idUsuario) -- Opcional pero útil
 );
 
+-- Insert 
+INSERT INTO Departamentos (departamento) VALUES 
+('CAPITAL'),
+('CALAMUCHITA'),
+('COLON'),
+('CRUZ DEL EJE'),
+('GENERAL ROCA'),
+('GENERAL SAN MARTIN'),
+('ISCHILIN'),
+('JUAREZ CELMAN'),
+('MARCOS JUAREZ'),
+('MINAS'),
+('POCHO'),
+('PTE. ROQUE SAENZ PEÑA'),
+('PUNILLA'),
+('RIO CUARTO'),
+('RIO PRIMERO'),
+('RIO SECO'),
+('RIO SEGUNDO'),
+('SAN ALBERTO'),
+('SAN JAVIER'),
+('SAN JUSTO'),
+('SANTA MARIA'),
+('SOBREMONTE'),
+('TERCERO ARRIBA'),
+('TOTORAL'),
+('TULUMBA'),
+('UNION');
+
+INSERT INTO Oficios (oficio) VALUES 
+('GASISTA'),
+('ELECTRICISTA'),
+('PLOMERO'),
+('CARPINTERO'),
+('PINTOR'),
+('EMPLEADA DOMESTICA'),
+('INSTALADOR DE AIRES ACONDICIONADOS');
+
 -- Índices para performance
 CREATE INDEX idx_reset_token_email ON password_reset_tokens(email);
 CREATE INDEX idx_reset_token_expiry ON password_reset_tokens(expiry_date);
-
-
--- Datos iniciales
-INSERT INTO Departamentos (departamento) VALUES
-                                             ('CAPITAL'),
-                                             ('CALAMUCHITA'),
-                                             ('COLON'),
-                                             ('CRUZ DEL EJE'),
-                                             ('GENERAL ROCA'),
-                                             ('GENERAL SAN MARTIN'),
-                                             ('ISCHILIN'),
-                                             ('JUAREZ CELMAN'),
-                                             ('MARCOS JUAREZ'),
-                                             ('MINAS'),
-                                             ('POCHO'),
-                                             ('PTE. ROQUE SAENZ PEÑA'),
-                                             ('PUNILLA'),
-                                             ('RIO CUARTO'),
-                                             ('RIO PRIMERO'),
-                                             ('RIO SECO'),
-                                             ('RIO SEGUNDO'),
-                                             ('SAN ALBERTO'),
-                                             ('SAN JAVIER'),
-                                             ('SAN JUSTO'),
-                                             ('SANTA MARIA'),
-                                             ('SOBREMONTE'),
-                                             ('TERCERO ARRIBA'),
-                                             ('TOTORAL'),
-                                             ('TULUMBA'),
-                                             ('UNION');
-
-INSERT INTO Oficios (oficio) VALUES
-                                 ('GASISTA'),
-                                 ('ELECTRICISTA'),
-                                 ('PLOMERO'),
-                                 ('CARPINTERO'),
-                                 ('PINTOR'),
-                                 ('EMPLEADA DOMESTICA'),
-                                 ('INSTALADOR DE AIRES ACONDICIONADOS');
