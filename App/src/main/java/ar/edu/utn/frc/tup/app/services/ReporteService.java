f233ab3b66328a0b6ca5a77d166af2b84340e27d
114039_PASTORJONAS <114039@tecnicatura.frc.utn.edu.ar>
2025-11-16 00:03:57 -0300

    Update

---
+++ App/src/main/java/ar/edu/utn/frc/tup/app/services/ReporteService.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frc.tup.app.services;
+
+public interface ReporteService {
+}
---
+++ App/src/main/java/ar/edu/utn/frc/tup/app/services/impl/ReporteServiceImpl.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frc.tup.app.services.impl;
+
+import ar.edu.utn.frc.tup.app.services.ReporteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReporteServiceImpl implements ReporteService {
+}
