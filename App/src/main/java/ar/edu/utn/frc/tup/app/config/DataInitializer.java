06f457004c1b128e5b42ff3798d3fdb2949b1d37
114039_PASTORJONAS <114039@tecnicatura.frc.utn.edu.ar>
2026-01-16 18:49:01 -0300

    Update DataInitializer.java

--- App/src/main/java/ar/edu/utn/frc/tup/app/config/DataInitializer.java
+++ App/src/main/java/ar/edu/utn/frc/tup/app/config/DataInitializer.java
@@ -1,45 +1,19 @@
 package ar.edu.utn.frc.tup.app.config;
 
-import ar.edu.utn.frc.tup.app.entities.TiposDocumento;
-import ar.edu.utn.frc.tup.app.repositories.TipoDocumentoRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
+/**
+ * DataInitializer - Disabled because Flyway migrations handle all initial data.
+ * Initial data is inserted via V1__Initial_Schema.sql migration script.
+ */
 @Slf4j
+// @Component - Commented out to disable this initializer
 public class DataInitializer implements CommandLineRunner {
-
-    private final TipoDocumentoRepository tipoDocumentoRepository;
-    private final Flyway flyway; // Ensure Flyway runs first
 
     @Override
     public void run(String... args) throws Exception {
-        initializeTiposDocumento();
-    }
-
-    private void initializeTiposDocumento() {
-        if (tipoDocumentoRepository.count() == 0) {
-            log.info("Inicializando tipos de documento...");
-            
-            TiposDocumento dni = new TiposDocumento();
-            dni.setTipo("DNI");
-            tipoDocumentoRepository.save(dni);
-
-            TiposDocumento cuil = new TiposDocumento();
-            cuil.setTipo("CUIL");
-            tipoDocumentoRepository.save(cuil);
-
-            TiposDocumento pasaporte = new TiposDocumento();
-            pasaporte.setTipo("PASAPORTE");
-            tipoDocumentoRepository.save(pasaporte);
-
-            log.info("Tipos de documento inicializados correctamente");
-        } else {
-            log.info("Tipos de documento ya existen en la base de datos");
-        }
+        log.info("DataInitializer disabled - data initialization handled by Flyway migrations");
     }
 }
