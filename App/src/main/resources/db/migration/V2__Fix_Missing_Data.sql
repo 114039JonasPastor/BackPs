e79bbdd633d93003b6daf3d736d52b75c5941317
114039_PASTORJONAS <114039@tecnicatura.frc.utn.edu.ar>
2026-01-19 18:50:34 -0300

    Remove V2 migration and enable Flyway clean for DB reset

--- App/src/main/resources/db/migration/V2__Fix_Missing_Data.sql
+++
@@ -1,9 +0,0 @@
--- Insertar tipos de documento si no existen
-INSERT INTO tipos_documento (tipo) 
-SELECT 'DNI' WHERE NOT EXISTS (SELECT 1 FROM tipos_documento WHERE tipo = 'DNI');
-
-INSERT INTO tipos_documento (tipo) 
-SELECT 'CUIL' WHERE NOT EXISTS (SELECT 1 FROM tipos_documento WHERE tipo = 'CUIL');
-
-INSERT INTO tipos_documento (tipo) 
-SELECT 'PASAPORTE' WHERE NOT EXISTS (SELECT 1 FROM tipos_documento WHERE tipo = 'PASAPORTE');
